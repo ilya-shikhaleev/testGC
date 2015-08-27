9ddba8a513497ee6b7082e8b101487cd1e381dcc
Ilya Shikhaleev <ilya.shikhaleev@cpslabs.net>
2015-08-27 14:39:08 +0300

    go fmt

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "runtime"
-    "time"
+	"log"
+	"net/http"
+	"runtime"
+	"time"
 )
 
 import _ "net/http/pprof"
@@ -14,37 +14,37 @@ var People []*Person
 const PeopleCount = 10000000
 
 type Person struct {
-    Name string
-    Age  uint
+	Name string
+	Age  uint
 }
 
 func allocateInitial() {
-    for i := 0; i < PeopleCount; i++ {
-        People = append(People, &Person{"Ilya", 24})
-    }
+	for i := 0; i < PeopleCount; i++ {
+		People = append(People, &Person{"Ilya", 24})
+	}
 }
 
 func allocateMore() {
-    m := runtime.MemStats{}
-    var prevTotalPause, currTotalPause uint64
-    for {
-        log.Println("Start allocating")
-        for i := 0; i < PeopleCount; i++ {
-            People = append(People, &Person{"Shikhaleev", 25})
-        }
-        People = People[0:PeopleCount]
-        runtime.GC()
-        runtime.ReadMemStats(&m)
-        currTotalPause = m.PauseTotalNs / 1000000 //Nanoseconds to milliseconds
-        log.Println("GC pause =", currTotalPause-prevTotalPause, "ms")
-        prevTotalPause = currTotalPause
-        time.Sleep(3 * time.Second)
-    }
+	m := runtime.MemStats{}
+	var prevTotalPause, currTotalPause uint64
+	for {
+		log.Println("Start allocating")
+		for i := 0; i < PeopleCount; i++ {
+			People = append(People, &Person{"Shikhaleev", 25})
+		}
+		People = People[0:PeopleCount]
+		runtime.GC()
+		runtime.ReadMemStats(&m)
+		currTotalPause = m.PauseTotalNs / 1000000 //Nanoseconds to milliseconds
+		log.Println("GC pause =", currTotalPause-prevTotalPause, "ms")
+		prevTotalPause = currTotalPause
+		time.Sleep(3 * time.Second)
+	}
 }
 
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())
-    allocateInitial()
-    go allocateMore()
-    log.Println(http.ListenAndServe("127.0.0.1:8080", nil))
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	allocateInitial()
+	go allocateMore()
+	log.Println(http.ListenAndServe("127.0.0.1:8080", nil))
 }
